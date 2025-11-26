8e589e47a4c29aaf944d4c7c8843f63eb789f5ee
ciprianb <ciprianb@indsoft.ro>
2025-11-26 14:41:43 +0200

    Add skeletons and search logic in invoices

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+// export default function Loading() {
+//   return <div>Loading...</div>;
+// }
+import LatestInvoicesSkeleton from "@/app/ui/skeletons";
+
+export default function Loading() {
+  return <LatestInvoicesSkeleton />;
+}
